ba54784c9837f3a323fb71eac732f57eb53d7506
Anubhav_722 <anubhavs286@gmail.com>
2017-01-26 13:04:51 +0530

    customize the admin

--- Scanner/QRCode/admin.py
+++ Scanner/QRCode/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import UserProfile
 # Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    exclude = ['qr_code', 'auth_token']
+
+admin.site.register(UserProfile, UserProfileAdmin)
