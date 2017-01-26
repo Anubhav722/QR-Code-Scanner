ffad2e05654028213602bd6182c869014ae64752
Anubhav_722 <anubhavs286@gmail.com>
2017-01-26 12:59:56 +0530

    add models and signals

--- Scanner/QRCode/models.py
+++ Scanner/QRCode/models.py
@@ -1,5 +1,50 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+import qrcode
+from django.utils.crypto import get_random_string
+import StringIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 # Create your models here.
+AUTH_TOKEN_LENGTH = 15
+
+def upload_location(instance, filename):
+    return "%s/%s" %(instance.id, filename)
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    qr_code = models.ImageField(upload_to=upload_location, null=True, blank=True)
+    auth_token = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return self.user.username
+
+@receiver(pre_save, sender = UserProfile)
+def qr_code_save_call_back(sender, instance, *args, **kwargs):
+    if not instance.qr_code:
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+
+        instance.auth_token = get_random_string(length=AUTH_TOKEN_LENGTH)
+        qr.add_data(instance.user.username, instance.auth_token)
+        qr.make(fit=True)
+
+        img = qr.make_image(fill_color="black", back_color="white")
+        buffer = StringIO.StringIO()
+        img.save(buffer)
+        filename = 'events-%s.png' % (instance.user.username)
+        filebuffer = InMemoryUploadedFile(
+            buffer, None, filename, 'image/png', buffer.len, None)
+
+        instance.qr_code.save(filename, filebuffer)
--- Scanner/Scanner/settings.py
+++ Scanner/Scanner/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #my_app
+    'QRCode',
+    'qrcode',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'static' ),
+    ]
+
+STATIC_ROOT=os.path.join(BASE_DIR, 'static_cdn')
+
+MEDIA_URL='/media/'
+
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media_cdn')
+
