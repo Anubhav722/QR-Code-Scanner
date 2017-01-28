e9ed061bd23976aa9cea1653bd7392402e01399c
Anubhav_722 <anubhavs286@gmail.com>
2017-01-28 11:14:01 +0530

    add model forms

---
+++ Scanner/QRCode/forms.py
@@ -0,0 +1,10 @@
+from .models import UserProfile
+from django import forms
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = [
+            'user',
+            'description',
+        ]
