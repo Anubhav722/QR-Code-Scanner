128bf73a4b3620ec41de14272bcedd2fcd12c696
Anubhav_722 <anubhavs286@gmail.com>
2017-01-28 11:32:32 +0530

    add the views

--- Scanner/QRCode/views.py
+++ Scanner/QRCode/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import FormView, CreateView
+from django.views.generic.list import ListView
+from .models import UserProfile
+from .forms import UserProfileForm
 # Create your views here.
+#
+# class UserCreate(CreateView):
+#     form_class = UserProfileForm
+#     model = UserProfile
+#
+#     fields = ['user', 'description']
+
+class IndexView(ListView):
+    queryset = UserProfile.objects.all()
+    template_name = 'index.html'
+    # by default the name of the template will be tweet_list.html
+    def get_context_data(self, *args, **kwargs):
+        context= super(IndexView, self).get_context_data(*args, **kwargs)
+        #print (context)
+        return context
+
